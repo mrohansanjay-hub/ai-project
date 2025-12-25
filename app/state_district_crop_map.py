837774c0af55fd4805b2edee777bcb2568fe9f30
mrohansanjay-hub <mrohansanjay@gmail.com>
2025-12-25 15:23:25 +0530

    new

---
+++ app/state_district_crop_map.py
@@ -0,0 +1,136 @@
+STATE_CROP_MAP = {
+    {    "Andhra Pradesh":
+        {
+            "Srikakulam": {
+                "kharif": ["rice", "maize", "groundnut", "pulses"],
+                "rabi": ["rice", "maize", "pulses"],
+                "summer": ["pulses"]
+            },
+            "Vizianagaram": {
+                "kharif": ["rice", "maize", "groundnut"],
+                "rabi": ["rice", "maize", "pulses"],
+                "summer": ["pulses"]
+            },
+            "Parvathipuram Manyam": {
+                "kharif": ["maize", "pulses"],
+                "rabi": ["pulses"],
+                "summer": []
+            },
+            "Alluri Sitharama Raju": {
+                "kharif": ["rice", "maize"],
+                "rabi": ["maize", "pulses"],
+                "summer": []
+            },
+            "Visakhapatnam": {
+                "kharif": ["rice", "maize", "pulses"],
+                "rabi": ["rice", "pulses"],
+                "summer": ["maize"]
+            },
+            "Anakapalli": {
+                "kharif": ["rice", "maize", "groundnut", "pulses"],
+                "rabi": ["rice", "maize", "pulses"],
+                "summer": ["pulses"]
+            },
+            "Kakinada": {
+                "kharif": ["rice", "maize", "pulses"],
+                "rabi": ["rice", "maize", "pulses"],
+                "summer": ["pulses"]
+            },
+            "Dr. B. R. Ambedkar Konaseema": {
+                "kharif": ["rice", "maize", "pulses"],
+                "rabi": ["rice", "pulses"],
+                "summer": ["pulses"]
+            },
+            "East Godavari": {
+                "kharif": ["rice", "maize", "pulses"],
+                "rabi": ["rice", "pulses"],
+                "summer": ["pulses"]
+            },
+            "Eluru": {
+                "kharif": ["rice", "maize", "pulses"],
+                "rabi": ["rice", "maize", "pulses"],
+                "summer": ["pulses"]
+            },
+            "West Godavari": {
+                "kharif": ["rice", "maize"],
+                "rabi": ["rice", "maize", "pulses"],
+                "summer": ["pulses"]
+            },
+            "Krishna": {
+                "kharif": ["rice", "maize", "cotton", "pulses"],
+                "rabi": ["rice", "maize", "pulses"],
+                "summer": ["pulses"]
+            },
+            "NTR": {
+                "kharif": ["rice", "maize", "cotton", "pulses"],
+                "rabi": ["rice", "maize", "pulses"],
+                "summer": ["pulses"]
+            },
+            "Guntur": {
+                "kharif": ["rice", "maize", "cotton"],
+                "rabi": ["rice", "pulses"],
+                "summer": ["pulses"]
+            },
+            "Bapatla": {
+                "kharif": ["rice", "maize", "pulses"],
+                "rabi": ["rice", "pulses"],
+                "summer": ["pulses"]
+            },
+            "Palnadu": {
+                "kharif": ["maize", "cotton", "pulses"],
+                "rabi": ["pulses"],
+                "summer": []
+            },
+            "Prakasam": {
+                "kharif": ["groundnut", "cotton", "pulses"],
+                "rabi": ["pulses"],
+                "summer": []
+            },
+            "SPSR Nellore": {
+                "kharif": ["rice", "groundnut", "pulses"],
+                "rabi": ["rice", "groundnut", "pulses"],
+                "summer": ["groundnut", "pulses"]
+            },
+            "Tirupati": {
+                "kharif": ["rice", "groundnut", "pulses"],
+                "rabi": ["rice", "groundnut", "pulses"],
+                "summer": ["groundnut"]
+            },
+            "Chittoor": {
+                "kharif": ["rice", "groundnut"],
+                "rabi": ["rice", "groundnut", "pulses"],
+                "summer": ["groundnut"]
+            },
+            "Annamayya": {
+                "kharif": ["groundnut", "pulses"],
+                "rabi": ["groundnut", "pulses"],
+                "summer": []
+            },
+            "YSR Kadapa": {
+                "kharif": ["groundnut", "cotton", "pulses"],
+                "rabi": ["pulses"],
+                "summer": []
+            },
+            "Kurnool": {
+                "kharif": ["rice", "groundnut", "cotton", "pulses"],
+                "rabi": ["pulses"],
+                "summer": ["pulses"]
+            },
+            "Nandyal": {
+                "kharif": ["rice", "groundnut", "cotton", "pulses"],
+                "rabi": ["pulses"],
+                "summer": []
+            },
+            "Anantapuramu": {
+                "kharif": ["groundnut", "pulses"],
+                "rabi": ["groundnut", "pulses"],
+                "summer": []
+            },
+            "Sri Sathya Sai": {
+                "kharif": ["groundnut", "pulses"],
+                "rabi": ["groundnut", "pulses"],
+                "summer": []
+            }
+        }
+    }
+}
